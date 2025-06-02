9d095786daa5006f4c670190a92c59ccdfb4426f
AbhijithS98 <abhijithsb745@gmail.com>
2025-06-03 00:27:57 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <section className="h-screen bg-green-400 flex items-center justify-center m-12 text-center text-3xl p-12 md:p-14 lg:p-16 xl:p-20">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </section>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="bg-gray-900 text-white px-6 py-4 shadow-md">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <Link to="/" className="text-3xl font-bold tracking-wide text-indigo-400">
+          Log Viewer
+        </Link>
+        <div className="space-x-4 hidden sm:flex">
+          <Link to="/" className="hover:text-indigo-300 transition-colors">
+            Home
+          </Link>
+          
+        </div>
+      </div>
+    </nav>
+    </div>
+  )
+}
+
+export default Navbar
